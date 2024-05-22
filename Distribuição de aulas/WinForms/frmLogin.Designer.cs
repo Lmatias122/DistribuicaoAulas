870fca17c07146a039a9ed0eb319112ad7a9ff2c
Kayuan165 <kayuandiasoli@gmail.com>
2024-05-22 20:04:21 -0300

    teste3

--- Distribuição de aulas/WinForms/frmLogin.Designer.cs
+++ Distribuição de aulas/WinForms/frmLogin.Designer.cs
@@ -36,6 +36,7 @@
             this.txtSenha = new System.Windows.Forms.TextBox();
             this.checkSenha = new System.Windows.Forms.CheckBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnCancelar
@@ -124,6 +125,16 @@
             this.label1.TabIndex = 8;
             this.label1.Text = "Visualizar Senha";
             // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(189, 3);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 31);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Nome";
+            // 
             // frmLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -131,6 +142,7 @@
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.ClientSize = new System.Drawing.Size(629, 336);
             this.ControlBox = false;
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.checkSenha);
             this.Controls.Add(this.txtSenha);
@@ -159,5 +171,6 @@
         private System.Windows.Forms.TextBox txtSenha;
         private System.Windows.Forms.CheckBox checkSenha;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
