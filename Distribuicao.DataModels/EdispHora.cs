e2f78b156ec294e395f1ffec2c2dbb35ca95c499
Leonardo <leomatias.lm@gmail.com>
2024-06-03 22:17:00 -0300

    Adicionei a logica da pagina de cadastros de usuarios

---
+++ Distribuicao.DataModels/EdispHora.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Distribuicao.DataModels
+{
+    public enum EdispHora
+    {
+        primeiro,
+        segundo
+
+    }
+}
--- Distribuição de aulas/WinForms/frmCadastrousuarios.Designer.cs
+++ Distribuição de aulas/WinForms/frmCadastrousuarios.Designer.cs
@@ -33,26 +33,25 @@
             this.textNome = new System.Windows.Forms.TextBox();
             this.lblNome = new System.Windows.Forms.Label();
             this.lblPeriodo = new System.Windows.Forms.Label();
-            this.checkNoite = new System.Windows.Forms.CheckBox();
-            this.checkTarde = new System.Windows.Forms.CheckBox();
-            this.checkManha = new System.Windows.Forms.CheckBox();
             this.btnVoltar = new System.Windows.Forms.Button();
-            this.cmbEmail = new System.Windows.Forms.ComboBox();
-            this.cmbDisponibilidade = new System.Windows.Forms.ComboBox();
             this.lblSenha = new System.Windows.Forms.Label();
             this.lblCargo = new System.Windows.Forms.Label();
             this.textSenha = new System.Windows.Forms.TextBox();
             this.cmbCargo = new System.Windows.Forms.ComboBox();
             this.btnCadastro = new System.Windows.Forms.Button();
+            this.textEmail = new System.Windows.Forms.TextBox();
+            this.checkDisp_dia = new System.Windows.Forms.CheckedListBox();
+            this.checkDisp_Hora = new System.Windows.Forms.CheckedListBox();
             this.SuspendLayout();
             // 
             // lblEmail
             // 
             this.lblEmail.AutoSize = true;
             this.lblEmail.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblEmail.Location = new System.Drawing.Point(4, 276);
+            this.lblEmail.Location = new System.Drawing.Point(3, 177);
+            this.lblEmail.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblEmail.Name = "lblEmail";
-            this.lblEmail.Size = new System.Drawing.Size(66, 28);
+            this.lblEmail.Size = new System.Drawing.Size(55, 23);
             this.lblEmail.TabIndex = 29;
             this.lblEmail.Text = "Email";
             // 
@@ -60,9 +59,10 @@
             // 
             this.lblDisponibilidade.AutoSize = true;
             this.lblDisponibilidade.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDisponibilidade.Location = new System.Drawing.Point(4, 215);
+            this.lblDisponibilidade.Location = new System.Drawing.Point(386, 235);
+            this.lblDisponibilidade.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblDisponibilidade.Name = "lblDisponibilidade";
-            this.lblDisponibilidade.Size = new System.Drawing.Size(164, 28);
+            this.lblDisponibilidade.Size = new System.Drawing.Size(136, 23);
             this.lblDisponibilidade.TabIndex = 28;
             this.lblDisponibilidade.Text = "Disponibilidade";
             // 
@@ -72,18 +72,20 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.textNome.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textNome.Location = new System.Drawing.Point(278, 13);
+            this.textNome.Location = new System.Drawing.Point(208, 11);
+            this.textNome.Margin = new System.Windows.Forms.Padding(2);
             this.textNome.Name = "textNome";
-            this.textNome.Size = new System.Drawing.Size(579, 32);
+            this.textNome.Size = new System.Drawing.Size(435, 27);
             this.textNome.TabIndex = 27;
             // 
             // lblNome
             // 
             this.lblNome.AutoSize = true;
             this.lblNome.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNome.Location = new System.Drawing.Point(4, 17);
+            this.lblNome.Location = new System.Drawing.Point(3, 14);
+            this.lblNome.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblNome.Name = "lblNome";
-            this.lblNome.Size = new System.Drawing.Size(70, 28);
+            this.lblNome.Size = new System.Drawing.Size(59, 23);
             this.lblNome.TabIndex = 25;
             this.lblNome.Text = "Nome";
             // 
@@ -91,97 +93,34 @@
             // 
             this.lblPeriodo.AutoSize = true;
             this.lblPeriodo.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPeriodo.Location = new System.Drawing.Point(4, 359);
+            this.lblPeriodo.Location = new System.Drawing.Point(78, 235);
+            this.lblPeriodo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblPeriodo.Name = "lblPeriodo";
-            this.lblPeriodo.Size = new System.Drawing.Size(87, 28);
+            this.lblPeriodo.Size = new System.Drawing.Size(72, 23);
             this.lblPeriodo.TabIndex = 36;
             this.lblPeriodo.Text = "Período";
-            // 
-            // checkNoite
-            // 
-            this.checkNoite.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.checkNoite.AutoSize = true;
-            this.checkNoite.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.checkNoite.Location = new System.Drawing.Point(778, 361);
-            this.checkNoite.Name = "checkNoite";
-            this.checkNoite.Size = new System.Drawing.Size(79, 28);
-            this.checkNoite.TabIndex = 35;
-            this.checkNoite.Text = "Noite";
-            this.checkNoite.UseVisualStyleBackColor = true;
-            // 
-            // checkTarde
-            // 
-            this.checkTarde.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.checkTarde.AutoSize = true;
-            this.checkTarde.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.checkTarde.Location = new System.Drawing.Point(540, 359);
-            this.checkTarde.Name = "checkTarde";
-            this.checkTarde.Size = new System.Drawing.Size(84, 28);
-            this.checkTarde.TabIndex = 33;
-            this.checkTarde.Text = "Tarde";
-            this.checkTarde.UseVisualStyleBackColor = true;
-            // 
-            // checkManha
-            // 
-            this.checkManha.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.checkManha.AutoSize = true;
-            this.checkManha.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.checkManha.Location = new System.Drawing.Point(278, 359);
-            this.checkManha.Name = "checkManha";
-            this.checkManha.Size = new System.Drawing.Size(91, 28);
-            this.checkManha.TabIndex = 33;
-            this.checkManha.Text = "Manhã";
-            this.checkManha.UseVisualStyleBackColor = true;
             // 
             // btnVoltar
             // 
             this.btnVoltar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnVoltar.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnVoltar.Location = new System.Drawing.Point(764, 516);
+            this.btnVoltar.Location = new System.Drawing.Point(573, 419);
+            this.btnVoltar.Margin = new System.Windows.Forms.Padding(2);
             this.btnVoltar.Name = "btnVoltar";
-            this.btnVoltar.Size = new System.Drawing.Size(104, 47);
+            this.btnVoltar.Size = new System.Drawing.Size(78, 38);
             this.btnVoltar.TabIndex = 37;
             this.btnVoltar.Text = "Voltar";
             this.btnVoltar.UseVisualStyleBackColor = true;
             this.btnVoltar.Click += new System.EventHandler(this.btnVoltar_Click);
             // 
-            // cmbEmail
-            // 
-            this.cmbEmail.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.cmbEmail.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cmbEmail.FormattingEnabled = true;
-            this.cmbEmail.Location = new System.Drawing.Point(278, 272);
-            this.cmbEmail.Name = "cmbEmail";
-            this.cmbEmail.Size = new System.Drawing.Size(579, 32);
-            this.cmbEmail.TabIndex = 39;
-            // 
-            // cmbDisponibilidade
-            // 
-            this.cmbDisponibilidade.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.cmbDisponibilidade.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cmbDisponibilidade.FormattingEnabled = true;
-            this.cmbDisponibilidade.Location = new System.Drawing.Point(278, 211);
-            this.cmbDisponibilidade.Name = "cmbDisponibilidade";
-            this.cmbDisponibilidade.Size = new System.Drawing.Size(579, 32);
-            this.cmbDisponibilidade.TabIndex = 40;
-            // 
             // lblSenha
             // 
             this.lblSenha.AutoSize = true;
             this.lblSenha.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSenha.Location = new System.Drawing.Point(4, 80);
+            this.lblSenha.Location = new System.Drawing.Point(3, 65);
+            this.lblSenha.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblSenha.Name = "lblSenha";
-            this.lblSenha.Size = new System.Drawing.Size(75, 28);
+            this.lblSenha.Size = new System.Drawing.Size(63, 23);
             this.lblSenha.TabIndex = 41;
             this.lblSenha.Text = "Senha";
             // 
@@ -189,9 +128,10 @@
             // 
             this.lblCargo.AutoSize = true;
             this.lblCargo.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCargo.Location = new System.Drawing.Point(4, 151);
+            this.lblCargo.Location = new System.Drawing.Point(3, 123);
+            this.lblCargo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblCargo.Name = "lblCargo";
-            this.lblCargo.Size = new System.Drawing.Size(71, 28);
+            this.lblCargo.Size = new System.Drawing.Size(59, 23);
             this.lblCargo.TabIndex = 42;
             this.lblCargo.Text = "Cargo";
             // 
@@ -201,9 +141,10 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.textSenha.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textSenha.Location = new System.Drawing.Point(278, 76);
+            this.textSenha.Location = new System.Drawing.Point(208, 62);
+            this.textSenha.Margin = new System.Windows.Forms.Padding(2);
             this.textSenha.Name = "textSenha";
-            this.textSenha.Size = new System.Drawing.Size(579, 32);
+            this.textSenha.Size = new System.Drawing.Size(435, 27);
             this.textSenha.TabIndex = 43;
             // 
             // cmbCargo
@@ -213,48 +154,83 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.cmbCargo.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmbCargo.FormattingEnabled = true;
-            this.cmbCargo.Location = new System.Drawing.Point(278, 147);
+            this.cmbCargo.Location = new System.Drawing.Point(208, 119);
+            this.cmbCargo.Margin = new System.Windows.Forms.Padding(2);
             this.cmbCargo.Name = "cmbCargo";
-            this.cmbCargo.Size = new System.Drawing.Size(579, 32);
+            this.cmbCargo.Size = new System.Drawing.Size(435, 27);
             this.cmbCargo.TabIndex = 44;
             // 
             // btnCadastro
             // 
             this.btnCadastro.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.btnCadastro.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCadastro.Location = new System.Drawing.Point(540, 516);
+            this.btnCadastro.Location = new System.Drawing.Point(405, 419);
+            this.btnCadastro.Margin = new System.Windows.Forms.Padding(2);
             this.btnCadastro.Name = "btnCadastro";
-            this.btnCadastro.Size = new System.Drawing.Size(120, 47);
+            this.btnCadastro.Size = new System.Drawing.Size(90, 38);
             this.btnCadastro.TabIndex = 12;
             this.btnCadastro.Text = "Cadastrar";
             this.btnCadastro.UseVisualStyleBackColor = true;
+            this.btnCadastro.Click += new System.EventHandler(this.btnCadastro_Click);
+            // 
+            // textEmail
+            // 
+            this.textEmail.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textEmail.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textEmail.Location = new System.Drawing.Point(208, 177);
+            this.textEmail.Margin = new System.Windows.Forms.Padding(2);
+            this.textEmail.Name = "textEmail";
+            this.textEmail.Size = new System.Drawing.Size(435, 27);
+            this.textEmail.TabIndex = 45;
+            // 
+            // checkDisp_dia
+            // 
+            this.checkDisp_dia.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkDisp_dia.FormattingEnabled = true;
+            this.checkDisp_dia.HorizontalScrollbar = true;
+            this.checkDisp_dia.Location = new System.Drawing.Point(384, 261);
+            this.checkDisp_dia.Name = "checkDisp_dia";
+            this.checkDisp_dia.Size = new System.Drawing.Size(259, 114);
+            this.checkDisp_dia.TabIndex = 47;
+            // 
+            // checkDisp_Hora
+            // 
+            this.checkDisp_Hora.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkDisp_Hora.FormattingEnabled = true;
+            this.checkDisp_Hora.HorizontalScrollbar = true;
+            this.checkDisp_Hora.Location = new System.Drawing.Point(12, 261);
+            this.checkDisp_Hora.Name = "checkDisp_Hora";
+            this.checkDisp_Hora.Size = new System.Drawing.Size(259, 114);
+            this.checkDisp_Hora.TabIndex = 48;
             // 
             // frmCadastrousuarios
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(922, 587);
+            this.ClientSize = new System.Drawing.Size(692, 477);
+            this.Controls.Add(this.checkDisp_Hora);
+            this.Controls.Add(this.checkDisp_dia);
+            this.Controls.Add(this.textEmail);
             this.Controls.Add(this.btnCadastro);
             this.Controls.Add(this.cmbCargo);
             this.Controls.Add(this.textSenha);
             this.Controls.Add(this.lblCargo);
             this.Controls.Add(this.lblSenha);
-            this.Controls.Add(this.cmbDisponibilidade);
-            this.Controls.Add(this.cmbEmail);
             this.Controls.Add(this.btnVoltar);
             this.Controls.Add(this.lblPeriodo);
-            this.Controls.Add(this.checkNoite);
-            this.Controls.Add(this.checkTarde);
-            this.Controls.Add(this.checkManha);
             this.Controls.Add(this.lblEmail);
             this.Controls.Add(this.lblDisponibilidade);
             this.Controls.Add(this.textNome);
             this.Controls.Add(this.lblNome);
-            this.MaximumSize = new System.Drawing.Size(1000, 700);
-            this.MinimumSize = new System.Drawing.Size(916, 574);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximumSize = new System.Drawing.Size(754, 576);
+            this.MinimumSize = new System.Drawing.Size(691, 474);
             this.Name = "frmCadastrousuarios";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Cadastrousuarios";
+            this.Load += new System.EventHandler(this.frmCadastrousuarios_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -266,16 +242,14 @@
         private System.Windows.Forms.TextBox textNome;
         private System.Windows.Forms.Label lblNome;
         private System.Windows.Forms.Label lblPeriodo;
-        private System.Windows.Forms.CheckBox checkNoite;
-        private System.Windows.Forms.CheckBox checkTarde;
-        private System.Windows.Forms.CheckBox checkManha;
         private System.Windows.Forms.Button btnVoltar;
-        private System.Windows.Forms.ComboBox cmbEmail;
-        private System.Windows.Forms.ComboBox cmbDisponibilidade;
         private System.Windows.Forms.Label lblSenha;
         private System.Windows.Forms.Label lblCargo;
         private System.Windows.Forms.TextBox textSenha;
         private System.Windows.Forms.ComboBox cmbCargo;
         private System.Windows.Forms.Button btnCadastro;
+        private System.Windows.Forms.TextBox textEmail;
+        private System.Windows.Forms.CheckedListBox checkDisp_dia;
+        private System.Windows.Forms.CheckedListBox checkDisp_Hora;
     }
 }
